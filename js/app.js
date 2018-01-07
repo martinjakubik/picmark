9ebe47244296caeab683cd0cb2b90f38bee2f389
martin jakubik <martin.jakubik@gmail.com>
2018-01-07 21:23:59 +0100

    es6'd some more

--- js/app.js
+++ js/app.js
@@ -1,109 +1,125 @@
 class Picmark {
 
-};
+    static imageFileSelected () {
 
-Picmark.imageFileSelected = function () {
+        var oInputImageFile = document.getElementById('inputImageFile');
 
-    var oInputImageFile = document.getElementById('inputImageFile');
+        var aImageFiles = oInputImageFile.files;
+        var oImageFile;
 
-    var aImageFiles = oInputImageFile.files;
-    var oImageFile;
+        var oElementCanvasImage = document.getElementById('canvasImage');
 
-    var oElementCanvasImage = document.getElementById('canvasImage');
+        var oAuth = firebase.auth();
+        var oStorageRef = firebase.storage().ref();
 
-    var oAuth = firebase.auth();
-    var oStorageRef = firebase.storage().ref();
+        var fnPushFileToStorage = function (oImageFile) {
+            var oMetadata = {
+                'contentType': oImageFile.type
+            };
 
-    var fnPushFileToStorage = function (oImageFile) {
-        var oMetadata = {
-            'contentType': oImageFile.type
+            // pushes file to storage at child path
+            oStorageRef.child('images/' + oImageFile.name).put(oImageFile, oMetadata).then(function(oSnapshot) {
+
+                console.log('uploaded', oSnapshot.totalBytes, 'bytes');
+                console.log(oSnapshot.metadata);
+
+                var sUrl = oSnapshot.downloadURL;
+                console.log('file available at: ', sUrl);
+
+            }).catch(function(oError) {
+                console.error('upload failed: ', oError);
+            });
         };
 
-        // pushes file to storage at child path
-        oStorageRef.child('images/' + oImageFile.name).put(oImageFile, oMetadata).then(function(oSnapshot) {
+        var fnDrawImageOnCanvas = function (oImageFile) {
 
-            console.log('uploaded', oSnapshot.totalBytes, 'bytes');
-            console.log(oSnapshot.metadata);
+            var oCanvasContext = oElementCanvasImage.getContext('2d');
 
-            var sUrl = oSnapshot.downloadURL;
-            console.log('file available at: ', sUrl);
+            var oElementImg = document.getElementById('elementImage');
+            oElementImg.file = oImageFile;
 
-        }).catch(function(oError) {
-            console.error('upload failed: ', oError);
+            var oFileReader = new FileReader();
+
+            // sets the image source to the image data once the data is loaded
+            oFileReader.onload = (
+                function (oImage) {
+                    return function (e) {
+                        oImage.src = e.target.result;
+                    };
+                }(oElementImg)
+            );
+
+            // draws the images once it's loaded
+            oElementImg.onload = function () {
+                oCanvasContext.drawImage(oElementImg, 0, 0);
+            };
+
+            oFileReader.readAsDataURL(oImageFile);
+        };
+
+        if (aImageFiles.length === 1) {
+
+            oImageFile = aImageFiles[0];
+
+            fnDrawImageOnCanvas(oImageFile);
+            fnPushFileToStorage(oImageFile);
+        }
+    };
+
+    static bodyLoaded () {
+
+        var prevX = -31;
+        var prevY = -31;
+
+        var oElementCanvasImage = document.getElementById('canvasImage');
+
+        var nElementLeft = oElementCanvasImage.offsetLeft;
+        var nElementTop = oElementCanvasImage.offsetTop;
+
+        oElementCanvasImage.addEventListener('click', function (event) {
+
+            var x = event.pageX - nElementLeft;
+            var y = event.pageY - nElementTop;
+            var nRadius = 30;
+
+            var oCanvasContext = oElementCanvasImage.getContext('2d');
+
+            var oElementImg = document.getElementById('elementImage');
+
+            oCanvasContext.drawImage(oElementImg, 0, 0);
+
+            prevX = x;
+            prevY = y;
+
+            oCanvasContext.strokeStyle = 'red';
+            oCanvasContext.beginPath();
+            oCanvasContext.arc(x, y, nRadius, 0, 3 * Math.PI);
+            oCanvasContext.stroke();
+
+            var oButtonSubmit = document.getElementById('buttonSubmit');
+            if (!oButtonSubmit) {
+                oButtonSubmit = document.createElement('button');
+                oButtonSubmit.id = 'buttonSubmit';
+                oButtonSubmit.innerHTML = 'Submit your guess';
+                document.body.insertBefore(oButtonSubmit, null);
+            }
         });
     };
 
-    var fnDrawImageOnCanvas = function (oImageFile) {
+    static handleAuthenticationStateChange (oUser) {
 
-        var oCanvasContext = oElementCanvasImage.getContext('2d');
+        if (oUser) {
+            // user is signed in
+            var sDisplayName = oUser.displayName;
+            var sEmail = oUser.email;
+            var bEmailVerified = oUser.emailVerified;
+            var oPhotoUrl = oUser.photoURL;
+            var bIsAnonymous = oUser.isAnonymous;
+            var sUid = oUser.uid;
+            var oProviderData = oUser.providerData;
+        } else {
+            // user is signed out
 
-        var oElementImg = document.getElementById('elementImage');
-        oElementImg.file = oImageFile;
-
-        var oFileReader = new FileReader();
-
-        // sets the image source to the image data once the data is loaded
-        oFileReader.onload = (
-            function (oImage) {
-                return function (e) {
-                    oImage.src = e.target.result;
-                };
-            }(oElementImg)
-        );
-
-        // draws the images once it's loaded
-        oElementImg.onload = function () {
-            oCanvasContext.drawImage(oElementImg, 0, 0);
-        };
-
-        oFileReader.readAsDataURL(oImageFile);
-    };
-
-    if (aImageFiles.length === 1) {
-
-        oImageFile = aImageFiles[0];
-
-        fnDrawImageOnCanvas(oImageFile);
-        fnPushFileToStorage(oImageFile);
-    }
-};
-
-Picmark.bodyLoaded = function () {
-
-    var prevX = -31;
-    var prevY = -31;
-
-    var oElementCanvasImage = document.getElementById('canvasImage');
-
-    var nElementLeft = oElementCanvasImage.offsetLeft;
-    var nElementTop = oElementCanvasImage.offsetTop;
-
-    oElementCanvasImage.addEventListener('click', function (event) {
-
-        var x = event.pageX - nElementLeft;
-        var y = event.pageY - nElementTop;
-        var nRadius = 30;
-
-        var oCanvasContext = oElementCanvasImage.getContext('2d');
-
-        var oElementImg = document.getElementById('elementImage');
-
-        oCanvasContext.drawImage(oElementImg, 0, 0);
-
-        prevX = x;
-        prevY = y;
-
-        oCanvasContext.strokeStyle = 'red';
-        oCanvasContext.beginPath();
-        oCanvasContext.arc(x, y, nRadius, 0, 3 * Math.PI);
-        oCanvasContext.stroke();
-
-        var oButtonSubmit = document.getElementById('buttonSubmit');
-        if (!oButtonSubmit) {
-            oButtonSubmit = document.createElement('button');
-            oButtonSubmit.id = 'buttonSubmit';
-            oButtonSubmit.innerHTML = 'Submit your guess';
-            document.body.insertBefore(oButtonSubmit, null);
         }
-    });
+    };
 };
